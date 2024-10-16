5b2ebe3941ed431f600fa2ee1daa75811bc2ad26
Hannah Caling <hgcaling17@gmail.com>
2024-10-16 20:38:44 +0800

    feat: upload university activity with new descriptive comments

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Restaurant {
     public static void main(String[] args) {
 
-
+        //List for restaurants' names
         ArrayList<String> restaurant_name_list = new ArrayList<>();
         restaurant_name_list.add("Vikings Luxury Buffet");
         restaurant_name_list.add("Antonio's Restaurant");
@@ -11,7 +11,7 @@ public class Restaurant {
         restaurant_name_list.add("Manam Comfort Filipino");
         restaurant_name_list.add("Ramen Nagi");
 
-
+        //List for restaurants' names
         ArrayList<String> restaurant_cuisinetype_list = new ArrayList<>();
         restaurant_cuisinetype_list.add("Buffet");
         restaurant_cuisinetype_list.add("Fine Dining");
@@ -20,13 +20,14 @@ public class Restaurant {
         restaurant_cuisinetype_list.add("Japanese");
 
 
-
+        //List for restaurants' names
         ArrayList<Integer> restaurant_id_list = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
             restaurant_id_list.add(i);
         }
 
 
+        //List for restaurants' names
         ArrayList<String> restaurant_websiteorcontract_list = new ArrayList<>();
         restaurant_websiteorcontract_list.add("www.vikings.ph");
         restaurant_websiteorcontract_list.add("www.antoniosrestaurant.ph");
@@ -35,7 +36,7 @@ public class Restaurant {
         restaurant_websiteorcontract_list.add("www.ramennagi.com.ph");
 
 
-
+        //Print results
         for (int i = 0; i < restaurant_name_list.size(); i++) {
             System.out.println("ID: " + restaurant_id_list.get(i) + ", Name: " + restaurant_name_list.get(i) + ", Cuisine Type: " + restaurant_cuisinetype_list.get(i) + ", Website: " + restaurant_websiteorcontract_list.get(i));
         }
