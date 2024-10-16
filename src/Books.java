88bc580f091e797c081ba1ecc51ca3dd5b473a18
Hannah Caling <hgcaling17@gmail.com>
2024-10-16 20:35:24 +0800

    feat: upload books activity with descriptive comments

--- src/Books.java
+++ src/Books.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Books {
     public static void main(String[] args) {
 
-
+        //List for books' names
         ArrayList<String> book_title_list = new ArrayList<>();
         book_title_list.add("The Great Gatsby");
         book_title_list.add("To Kill a Mockingbird");
@@ -11,7 +11,7 @@ public class Books {
         book_title_list.add("The Catcher in the Rye");
         book_title_list.add("A Brief History of Time");
 
-
+        //List for books' names
         ArrayList<String> book_author_list = new ArrayList<>();
         book_author_list.add("F. Scott Fitzgerald");
         book_author_list.add("Harper Lee");
@@ -20,13 +20,13 @@ public class Books {
         book_author_list.add("Stephen Hawking");
 
 
-
+        //List for books' names
         ArrayList<Integer> book_id_list = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
             book_id_list.add(i);
         }
 
-
+        //List for books' names
         ArrayList<Integer> book_publishedyear_list = new ArrayList<>();
         book_publishedyear_list.add(1925);
         book_publishedyear_list.add(1960);
@@ -35,7 +35,7 @@ public class Books {
         book_publishedyear_list.add(1988);
 
 
-
+        //Print result
         for (int i = 0; i < book_title_list.size(); i++) {
             System.out.println("ID: " + book_id_list.get(i) + ", Name: " + book_title_list.get(i) + ", Author: " + book_author_list.get(i) + ", Published Year: " + book_publishedyear_list.get(i));
         }
